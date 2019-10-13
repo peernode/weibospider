9610c9616d9f5b401432cc582f826d88de4b263c
xujy <xujy@yilan.tv>
2019-10-13 22:48:22 +0800

    1. fix bug

--- FileDownloadPipeline.py
+++ FileDownloadPipeline.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from scrapy.pipelines.files import FilesPipeline
-from urlparse import urlparse
-from os.path import basename,dirname,join
 class FileDownloadPipeline(FilesPipeline):
     def file_path(self, request, response=None, info=None):
         # path=urlparse(request.url).path
         # temp=join(basename(dirname(path)),basename(path))
         # return '%s/%s' % (basename(dirname(path)), basename(path))
-        return request.url.split("?")[0].split("/")[-1]
+        return "full/{0}".format(request.url.split("?")[0].split("/")[-1])
